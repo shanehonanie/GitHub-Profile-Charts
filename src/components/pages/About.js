4645c0673d368798f953ffa516f966f098cae3cd
Shane Honanie <shaneh425@gmail.com>
2019-11-14 20:10:14 -0800

    Basic Github Search Functionality

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+	return (
+		<Fragment>
+			<h1>About This App</h1>
+			<p>App to Search Github users</p>
+			<p>Created by Shane Honanie</p>
+			<p>Version 1.0.0</p>
+		</Fragment>
+	);
+};
+
+export default About;
