5ea0359704475ab82293a808f4622a9d6dbadf1b
Shane Honanie <shaneh425@gmail.com>
2019-12-25 21:09:09 -0800

    About Section Updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,17 @@ import React, { Fragment } from 'react';
 
 const About = () => {
 	return (
-		<Fragment>
-			<h1>About This App</h1>
-			<p>App to search Github profiles with an enhanced view</p>
-			<p>Created by Shane Honanie</p>
-		</Fragment>
+		<div className='about-section'>
+			<p>This website uses React.js + SCSS + GitHub API.</p>
+			<p>Created by Shane Honanie.</p>
+			<a
+				href='https://www.shanehonanie.com'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				www.shanehonanie.com
+			</a>
+		</div>
 	);
 };
 
